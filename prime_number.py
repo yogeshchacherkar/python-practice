55093b52dc70ac6042f635c577ea707f7df4ab3a
Yogesh Chacherkar <yogeshchacherkar@hotmail.com>
2021-08-10 15:33:53 +0530

    prime number program added
    optimized solution for prime number check

---
+++ prime_number.py
@@ -0,0 +1,34 @@
+def is_prime(number: int):
+    if number in [2, 3]:
+        return True
+    elif number % 2 == 0:
+        return False
+    else:
+        for i in range(4, number // 2):
+            if number % i == 0:
+                return False
+        else:
+            return True
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            number = int(input("\nEnter a positive number: "))
+
+            if number < 2:
+                print("INVALID INPUT! Please enter value greater than 1.")
+                continue
+
+            elif is_prime(number):
+                print("%d is a prime number." % number)
+
+            else:
+                print("%d is NOT a prime number." % number)
+
+            if not input("\nDo you want to continue? (Y/n):  ").lower() in ['y', 'yes']:
+                print("\nGood Bye!")
+                break
+
+        except ValueError:
+            print("Please enter valid input!")
